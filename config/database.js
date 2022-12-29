8bed8c3fdbb4f9fc3b2f01708ad9ffb319f0e5ef
Gaduh Hartawan <79295669+Gaduh-Hartawan@users.noreply.github.com>
2022-12-29 20:09:15 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import mysql from "mysql";
 
 const db = mysql.createConnection({
-  host: "https://thoughtful-jade-sawfish.cyclic.app",
+  host: "https://cafe-app-pam.cyclic.app",
   user: "root",
   password: "",
   database: "uaspam",
